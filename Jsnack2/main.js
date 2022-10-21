ce6b5cc6bf3e8e2895b5cf0cb6100e62b4aa038c
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2022-10-21 12:13:00 +0200

    JSnack2 completo

--- Jsnack2/main.js
+++ Jsnack2/main.js
@@ -2,12 +2,26 @@ let names = ["MARCELINE", "Finn", "jake", "Bmo"]
 
 let correctedNames = []
 
+let lowerCaseName;
+
 let correctedName;
 
+let firstCapitalLetter;
+
 for(let i = 0; i< names.length; i++){
-    correctedName = names[i].toLowerCase();
+    lowerCaseName = names[i].toLowerCase();
+
+    firstCapitalLetter = lowerCaseName[0].toUpperCase()
+
+    for(let i = 0; i < lowerCaseName.length; i++){
+        if(i>0){
+            correctedName += lowerCaseName[i]
+        } else {
+            correctedName = firstCapitalLetter
+        }
+    }
+
     correctedNames.push(correctedName)
-    
 }
 
 console.log(correctedNames)
